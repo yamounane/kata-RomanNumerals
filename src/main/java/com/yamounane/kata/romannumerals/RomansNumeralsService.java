b24a5bd0c6eb3cc20df102b24a9c2c9a09e00fe8
Yassine Amounane <yassine.am@gmail.com>
2018-04-15 03:51:05 +0200

    REFACTOR & add multiple test case : GREEN

--- src/main/java/com/yamounane/kata/romannumerals/RomansNumeralsService.java
+++ src/main/java/com/yamounane/kata/romannumerals/RomansNumeralsService.java
@@ -6,42 +6,19 @@ package com.yamounane.kata.romannumerals;
  */
 public class RomansNumeralsService {
 
-	public String translate(int i) {
-		String result = "";
-		if (i < 4) {
-			for (int j = 0; i > j; j++) {
-				result += "I";
-			}
-			return result;
-		}
-		return translate(i, result);
-	}
+	private Integer[] decimals = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+	private String[] romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-	private String translate(int i, String translate) {
-		switch (i) {
-		case 4:
-			translate += "IV";
-			break;
-		case 5:
-			translate += "V";
-			break;
-		case 10:
-			translate += "X";
-			break;
-		case 50:
-			translate += "L";
-			break;
-		case 100:
-			translate += "C";
-			break;
-		case 500:
-			translate += "D";
-			break;
-		case 1000:
-			translate += "M";
-			break;
+	public String translate(int romanNumber) {
+		String translation = "";
+
+		for (int i = 0; i < decimals.length; i++) {
+			while (romanNumber % decimals[i] < romanNumber) {
+				translation += romans[i];
+				romanNumber -= decimals[i];
+			}
 		}
-		return translate;
+		return translation;
 	}
 
 }
--- src/test/java/com/yamounane/kata/romannumerals/RomanNumeralsTest.java
+++ src/test/java/com/yamounane/kata/romannumerals/RomanNumeralsTest.java
@@ -43,8 +43,38 @@ public class RomanNumeralsTest {
 	}
 
 	@Test
+	public void should_return_VI_when_6_is_passed() {
+		assertThat(romanNumeralsService.translate(6)).isEqualTo("VI");
+	}
+
+	@Test
+	public void should_return_VII_when_7_is_passed() {
+		assertThat(romanNumeralsService.translate(7)).isEqualTo("VII");
+	}
+
+	@Test
+	public void should_return_VIII_when_8_is_passed() {
+		assertThat(romanNumeralsService.translate(8)).isEqualTo("VIII");
+	}
+
+	@Test
+	public void should_return_IX_when_9_is_passed() {
+		assertThat(romanNumeralsService.translate(9)).isEqualTo("IX");
+	}
+
+	@Test
 	public void should_return_X_when_10_is_passed() {
 		assertThat(romanNumeralsService.translate(10)).isEqualTo("X");
+	}
+
+	@Test
+	public void should_return_XI_when_11_is_passed() {
+		assertThat(romanNumeralsService.translate(11)).isEqualTo("XI");
+	}
+
+	@Test
+	public void should_return_XLII_when_42_is_passed() {
+		assertThat(romanNumeralsService.translate(42)).isEqualTo("XLII");
 	}
 
 	@Test
@@ -53,8 +83,33 @@ public class RomanNumeralsTest {
 	}
 
 	@Test
+	public void should_return_XC_when_90_is_passed() {
+		assertThat(romanNumeralsService.translate(90)).isEqualTo("XC");
+	}
+
+	@Test
+	public void should_return_XCIV_when_94_is_passed() {
+		assertThat(romanNumeralsService.translate(94)).isEqualTo("XCIV");
+	}
+
+	@Test
+	public void should_return_XCV_when_95_is_passed() {
+		assertThat(romanNumeralsService.translate(95)).isEqualTo("XCV");
+	}
+
+	@Test
 	public void should_return_C_when_100_is_passed() {
 		assertThat(romanNumeralsService.translate(100)).isEqualTo("C");
+	}
+
+	@Test
+	public void should_return_CLXVII_when_167_is_passed() {
+		assertThat(romanNumeralsService.translate(167)).isEqualTo("CLXVII");
+	}
+
+	@Test
+	public void should_return_CD_when_400_is_passed() {
+		assertThat(romanNumeralsService.translate(400)).isEqualTo("CD");
 	}
 
 	@Test
@@ -63,8 +118,33 @@ public class RomanNumeralsTest {
 	}
 
 	@Test
+	public void should_return_DCLXXXVII_when_687_is_passed() {
+		assertThat(romanNumeralsService.translate(687)).isEqualTo("DCLXXXVII");
+	}
+
+	@Test
 	public void should_return_M_when_1000_is_passed() {
 		assertThat(romanNumeralsService.translate(1000)).isEqualTo("M");
+	}
+
+	@Test
+	public void should_return_MDLXXXVII_when_1587_is_passed() {
+		assertThat(romanNumeralsService.translate(1587)).isEqualTo("MDLXXXVII");
+	}
+
+	@Test
+	public void should_return_MMMCDL_when_2450_is_passed() {
+		assertThat(romanNumeralsService.translate(2450)).isEqualTo("MMCDL");
+	}
+
+	@Test
+	public void should_return_MMMCDL_when_2999_is_passed() {
+		assertThat(romanNumeralsService.translate(2999)).isEqualTo("MMCMXCIX");
+	}
+
+	@Test
+	public void should_return_MMM_when_3000_is_passed() {
+		assertThat(romanNumeralsService.translate(3000)).isEqualTo("MMM");
 	}
 
 }
